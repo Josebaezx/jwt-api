50f0a4e1e3af8fe8893a7359acda7d09660d9956
Josebaezx <jose_baez84@hotmail.com>
2024-03-15 21:24:10 -0300

    main primera configuracion http

---
+++ src/main/java/com/josebaezx/pruebassr/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.josebaezx.pruebassr.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()
+                )
+                .rememberMe(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
